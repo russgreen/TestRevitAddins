9001d330ab367c86230a9346f68315428f29c5fa
Russ Green <1886088+russgreen@users.noreply.github.com>
2024-07-14 19:38:15 +0100

    Introduce IServiceDemo and its implementation
    - Added `IServiceDemo` interface and `ServiceDemo` class to `HostingV8.Services`, encapsulating new service functionalities.
    - Modified `StartupCommand.cs` to utilize `IServiceDemo` through dependency injection, replacing initial logging with service method call.
    - Adjusted `HostingV8.csproj` formatting for XML elements, improving readability without affecting functionality.

--- HostingV8/Commands/StartupCommand.cs
+++ HostingV8/Commands/StartupCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI;
+using HostingV8.Services;
 using Microsoft.Extensions.Logging;
 using Nice3point.Revit.Toolkit.External;
 
@@ -13,9 +14,12 @@ public class StartupCommand : ExternalCommand
 {
     private readonly ILogger<StartupCommand> _logger = Host.GetService<ILogger<StartupCommand>>();
 
+    private IServiceDemo _serviceDemo = Host.GetService<IServiceDemo>();
+
     public override void Execute()
     {
-        _logger.LogInformation("Executing the StartupCommand");
+        _serviceDemo.DoSomething();
+        //_logger.LogInformation("Executing the StartupCommand");
 
         var td = new TaskDialog("Testing commands work:")
         {
---
+++ HostingV8/Services/ServiceDemo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostingV8.Services;
+internal class ServiceDemo : IServiceDemo
+{
+    private readonly ILogger<ServiceDemo> _logger;
+
+    public ServiceDemo(ILogger<ServiceDemo> logger)
+    {
+        _logger = logger;
+    }
+    public void DoSomething()
+    {
+        _logger.LogDebug("ServiceDemo.DoSomething");
+    }
+}
+
